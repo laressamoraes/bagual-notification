dcffe78e18187617808c3afdd9089740f10d0ef9
laressamoraes <laressa.luisy@hotmail.com>
2026-09-22 19:57:40 -0300

    fix: corrige o tipo do id de String para UUID

--- src/main/java/com/laressa/notification/repository/NotificationRepository.java
+++ src/main/java/com/laressa/notification/repository/NotificationRepository.java
@@ -4,6 +4,8 @@ import com.laressa.notification.domain.NotificationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface NotificationRepository extends JpaRepository<NotificationLog, String> {
+public interface NotificationRepository extends JpaRepository<NotificationLog, UUID> {
 }
